03bbd2f8d37ea0491dfc3c3076c6f98afdeef76f
Shaun Lloyd <shaun@lloydie.com.au>
2020-08-02 21:03:15 +1000

    Initial driver, just list devices and watch hotplug for inital commit

---
+++ src/driver.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+import usb1
+
+class usbDriver():
+    
+    PRODUCT_ID = 0x24f0
+    VENDOR_ID = '202b'
+
+    def __init__(self):
+        # connect
+        print('TODO: Initialize.')
+
+    def hotplug_register(self):
+        print('FIX: hotplug_register.')
+        with usb1.USBContext() as context:
+            if not context.hasCapability(usb1.CAP_HAS_HOTPLUG):
+                print('Hotplug support is missing. Please update your libusb version.')
+                return
+            print('Registering hotplug callback...')
+            opaque = context.hotplugRegisterCallback(self.hotplug_callback)
+            print('Callback registered. Monitoring events, ^C to exit')
+            try:
+                while True:
+                    context.handleEvents()
+            except (KeyboardInterrupt, SystemExit):
+                print('Exiting')
+
+    def hotplug_callback(self, context, device, event):
+        print("Hotplug %s: %s" % ({
+            usb1.HOTPLUG_EVENT_DEVICE_ARRIVED: 'arrived', 
+            usb1.HOTPLUG_EVENT_DEVICE_LEFT: 'left',}[event],device,))
+
+    def connect(self):
+        print('TODO: Connect.')
+
+    def list(self):
+        # currently just lists all, will add just kb later
+        with usb1.USBContext() as context:
+            for device in context.getDeviceIterator(skip_on_error=True):
+                print('ID %04x:%04x' % (device.getVendorID(), device.getProductID()), '->'.join(str(x) for x in ['Bus %03i' % (device.getBusNumber(), )] + device.getPortNumberList()), 'Device', device.getDeviceAddress())
+
+    def __del__(self):
+        print('TODO: Disconnect.')
+
+if __name__ == '__main__':
+    driver = usbDriver()
+    # List all usb devices
+    # driver.list()
+    
+    # Register hotplug
+    #driver.hotplug_register()
